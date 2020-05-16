5f12d7b1a46f6878c2e86fd522f8787e2b8d8938
emmanuel <emmvazquez@gmail.com>
2020-05-16 08:57:25 -0500

    se accede mediante ciclo a un array

--- src/Arreglos.java
+++ src/Arreglos.java
@@ -12,15 +12,31 @@ private int Numero;
 		x = 8734;
 		x1 = 23;
 		x2 = 345;
-		System.out.println(x);
+		//System.out.println(x);
 		
 		int arreglo[] = new int[3]; //tamaño es de 3
 		arreglo[0] = 8734; //posición 0
 		arreglo[1] = 23;
 		arreglo[2] = 345;
+		//System.out.println(arreglo[1]);
 		
 		
-		System.out.println(arreglo[1]);
+		int arreglo2[] = {8734, 23, 345,67, 547, 524, 9863, 87653,32};
+		
+		//System.out.println(arreglo2[2]);
+		arreglo2[2] = 27;   //asignamos un valor a una posición en específico 
+		//System.out.println(arreglo2[0] + ' ' +  arreglo2[1]  + ' ' + arreglo2[2] + ' ' +  arreglo2[3]);
+		
+		System.out.println(arreglo2.length);
+		
+		for(int i = 0; i < arreglo2.length; i ++) {
+			System.out.print(arreglo2[i] + ",");
+		}	
+		
+		
+		
+		
+		
 	}
 	
 	
