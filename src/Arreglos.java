68035bc1a7d89d96de0f9c8ec7d047055da571b1
emmanuel <emmvazquez@gmail.com>
2020-05-16 08:40:59 -0500

    Arrreglo básico entero

---
+++ src/Arreglos.java
@@ -0,0 +1,27 @@
+
+public class Arreglos {
+private int Numero;
+
+
+	public Arreglos() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String a[]) {
+		int x, x1, x2;
+		x = 8734;
+		x1 = 23;
+		x2 = 345;
+		System.out.println(x);
+		
+		int arreglo[] = new int[3]; //tamaño es de 3
+		arreglo[0] = 8734; //posición 0
+		arreglo[1] = 23;
+		arreglo[2] = 345;
+		
+		
+		System.out.println(arreglo[1]);
+	}
+	
+	
+}
